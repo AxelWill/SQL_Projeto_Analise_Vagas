f3ee03bfc3ad75cbbd51970e042c9b85cba9d594
AxelWill <axelwillie@gmail.com>
2024-06-02 21:47:05 -0300

    Query 2

---
+++ projeto_sql/2_top_paying_skills.sql
@@ -0,0 +1,63 @@
+/*
+Quais habilidades são necessárias para as vagas com melhores salários de analista de dados?
+-Usaremos como base a query 1, adicionando as habilidades necessárias para a vaga
+-Isso nos dará uma uma boa ideia de quais vagas com alto salário requerem quais habilidades,
+    ajudando pessoas procurando trabalho a saber quais habilidades aprimorar
+*/
+
+WITH top_paying_jobs AS(
+SELECT
+    job_id,
+    company_dim.name AS company_name,
+    job_title,
+    salary_year_avg
+FROM
+    job_postings_fact
+LEFT JOIN
+    company_dim on job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_location = 'Anywhere'
+    AND job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM skills_dim
+LEFT JOIN skills_job_dim
+ON skills_dim.skill_id = skills_job_dim.skill_id
+INNER JOIN top_paying_jobs ON skills_job_dim.job_id = top_paying_jobs.job_id;
+
+/*Agora, se quisermos ver quais competências pagam mais no brasil podemos fazer o seguinte*/
+
+WITH top_paying_jobs AS(
+SELECT
+    job_id,
+    company_dim.name AS company_name,
+    job_title,
+    salary_year_avg
+FROM
+    job_postings_fact
+LEFT JOIN
+    company_dim on job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_location LIKE '%Brazil%'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills
+FROM skills_dim
+LEFT JOIN skills_job_dim
+ON skills_dim.skill_id = skills_job_dim.skill_id
+INNER JOIN top_paying_jobs ON skills_job_dim.job_id = top_paying_jobs.job_id
+
+/*Vemos rapidamente que SQL continua aparecendo muito, além de python
