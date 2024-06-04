e29b845b2b1d4e398b66b7cd14dc52d1ab3a8297
AxelWill <axelwillie@gmail.com>
2024-06-04 18:57:28 -0300

    query 6 att

--- projeto_sql/6_top_paying_jobs_skills.sql
+++ projeto_sql/6_top_paying_jobs_skills.sql
@@ -34,5 +34,36 @@ ORDER BY frequencia DESC;
 
 /*
 Novamente podemos ver, SQL, e principalmente Python, aparecem no topo, mostrando que,
-embora haja alta procura desta ferramenta em cargos iniciais, ela se mantém com uma alta procura para as posições que pagam mais
+embora haja alta procura desta ferramenta em cargos iniciais, ela se mantém com uma alta procura para as posições que pagam mais.
+
+Agora vendo apenas os cargos de Data Analyst.
 */
+
+WITH top_paying_jobs AS(
+SELECT
+    job_id,
+    salary_year_avg
+FROM
+    job_postings_fact
+LEFT JOIN
+    company_dim on job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+    AND job_title_short = 'Data Analyst'
+ORDER BY
+    salary_year_avg DESC
+LIMIT 50
+)
+
+SELECT
+    skills,
+    count(skills_dim.skill_id) as frequencia
+FROM skills_dim
+LEFT JOIN skills_job_dim
+ON skills_dim.skill_id = skills_job_dim.skill_id
+INNER JOIN top_paying_jobs ON skills_job_dim.job_id = top_paying_jobs.job_id
+GROUP BY skills
+ORDER BY frequencia DESC;
+
+-- SQL se tornou ainda mais frequente, e Python diminui pouco sua procura.
